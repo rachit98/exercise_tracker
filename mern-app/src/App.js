9f84da48359e73c7435755434bb8aed8eb530c90
rachit98 <rachity@gmail.com>
2021-06-11 22:11:18 +0530

    Backend done

---
+++ mern-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
