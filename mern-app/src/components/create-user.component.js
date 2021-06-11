f62cbac9cd744648227732cfd400b861a78f9f70
rachit98 <rachity@gmail.com>
2021-06-12 00:54:34 +0530

    Components

---
+++ mern-app/src/components/create-user.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class CreateUser extends Component{
+    render() {
+        return (
+            <div>
+                <p> dummy4</p>
+            </div>
+        );
+    }
+}
---
+++ mern-app/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class EditExercise extends Component{
+    render() {
+        return (
+            <div>
+                <p> dummy2</p>
+            </div>
+        );
+    }
+}
